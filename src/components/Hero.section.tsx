42a6648ade8152e0cbd9a2b1b893107a2bbeb1c5
TheFatAndFurious <mrguerrilla@gmail.com>
2023-10-06 13:48:50 +0200

    ajout du bouton La Pastille Pour Ma Structure, besoin de rajouter le lien vers le mail de Patrick

--- src/components/Hero.section.tsx
+++ src/components/Hero.section.tsx
@@ -16,31 +16,36 @@ export const Hero = () => {
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
-    >
-      <div className="column fr-container">
-        <h1 className="white">
-          La pastille SNAP,
-          <br />
-          <q>Mes applications à portée de main !</q>
-        </h1>
-        <h4 className="white fontNormal">La pastille SNAP vous attend en bas a droite...</h4>
-        <div className="row align-center">
-          {/* <Input
-            className='mr-1 white'
-            label="Courriel"
-            
-            // hintText="Texte de description"
-            // state="default"
-            // stateRelatedMessage="Text de validation / d'explication de l'erreur"
-          /> */}
-          <div className="mt-3">
-          <FranceConnectButton />
-            <a href="https://snap-auth.numerique.gouv.fr/auth/realms/rizomo/login-actions/authenticate?execution=a7cf741d-1660-4ebc-97de-24ce3e672253&client_id=sso&tab_id=IC-kdFXAM00">
-              <p className="white">S'identifier avec son courriel</p>
-            </a>          
+      >
+        <div className="column full-width ninetyVH ">
+          <div className="row justify-end">
+            <button className="fr-btn">La pastille pour ma structure</button>
           </div>
+          <div className="column fr-container justify full-height">
+          <h1 className="white">
+            La pastille SNAP,
+            <br />
+            <q>Mes applications à portée de main !</q>
+          </h1>
+          <h4 className="white fontNormal">La pastille SNAP vous attend en bas a droite...</h4>
+          <div className="row align-center">
+            {/* <Input
+              className='mr-1 white'
+              label="Courriel"
+          
+              // hintText="Texte de description"
+              // state="default"
+              // stateRelatedMessage="Text de validation / d'explication de l'erreur"
+            /> */}
+            <div className="mt-3">
+            <FranceConnectButton />
+              <a href="https://snap-auth.numerique.gouv.fr/auth/realms/rizomo/login-actions/authenticate?execution=a7cf741d-1660-4ebc-97de-24ce3e672253&client_id=sso&tab_id=IC-kdFXAM00">
+                <p className="white">S'identifier avec son courriel</p>
+              </a>
+            </div>
+          </div>
+                </div>
         </div>
-      </div>
     </section>
   );
 };
