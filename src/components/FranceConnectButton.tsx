bdc91f090f1909689f8292cc1cfbc99a173b205b
David Panart <davidpanart@gmail.com>
2023-10-18 11:01:19 +0200

    Update FranceConnectButton.tsx

--- src/components/FranceConnectButton.tsx
+++ src/components/FranceConnectButton.tsx
@@ -6,7 +6,7 @@ export default function FranceConnectButton() {
 <div className="fr-connect-group">    
     <button className="fr-connect" onClick={() =>
                 router.push(
-                  "https://auth.agentconnect.gouv.fr/api/v2/interaction/1IByJ6L3_wWtwVeebHI1S"
+                  "https://snap-auth.numerique.gouv.fr/auth/realms/rizomo/protocol/openid-connect/auth?client_id=sso&redirect_uri=https://rizomo.numerique.gouv.fr/_oauth/keycloak&scope=openid&response_type=code&kc_idp_hint=agentconnect"
                 )}>        
         <span className="fr-connect__login">S&rsquo;identifier avec</span>        
         <span className="fr-connect__brand">FranceConnect</span>    
